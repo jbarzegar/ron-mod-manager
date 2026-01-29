e7647c2f46c8402c7193f5cc792e89018561edbf
James Barzegar <james.barzegar@gmail.com>
2026-01-28 22:27:35 -0500

    setup daisy-ui

--- ui/frontend/src/app.tsx
+++ ui/frontend/src/app.tsx
@@ -1,15 +1,18 @@
 import { h } from 'preact';
 import { useState } from "preact/hooks";
+import "./app.css"
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div id="App">
-      <div>
-        <h2>{count}</h2>
-        <button disabled={count <= 0} onClick={_ => setCount(c => c - 1)}>-</button>
-        <button onClick={_ => setCount(c => c + 1)}>+</button>
+      <div class="p-5 flex flex-col gap-3">
+        <h2 class="text-3xl">{count}</h2>
+        <div className="flex gap-2">
+          <button class="btn rounded-full" disabled={count <= 0} onClick={_ => setCount(c => c - 1)}>-</button>
+          <button class="btn rounded-full" onClick={_ => setCount(c => c + 1)}>+</button>
+        </div>
       </div>
     </div>
   )
