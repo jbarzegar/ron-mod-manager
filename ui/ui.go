47eaa0c4e95cfd69ac6efc02899bce9a52e6f772
James Barzegar <james.barzegar@gmail.com>
2026-01-31 14:37:53 -0500

    move around files
    This enables the UI to access packages from the rest of the
    project. In throry grpc server's aren't going to be required at
    this point

--- ui/ui.go
+++ ui/ui.go
@@ -1,4 +1,4 @@
-package main
+package ui
 
 import (
 	"embed"
@@ -8,24 +8,24 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-//go:embed all:frontend/dist
+//go:embed all:dist
 var assets embed.FS
 
-func main() {
+func Init() {
 	// Create an instance of the app structure
 	app := NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "ron-mod-manager",
-		Width:  1024,
-		Height: 768,
+		Title: "ron-mod-manager",
+		// Width:  1024,
+		// Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		Bind: []any{
 			app,
 		},
 	})
