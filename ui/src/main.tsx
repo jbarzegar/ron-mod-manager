6a2420681b1aeec81efe12a2213c09b5ee219195
James Barzegar <james.barzegar@gmail.com>
2026-02-01 14:30:06 -0500

    setup biome (#14)
    * setup intial biome config and run format
    
    * setup workflow for linting
    
    * set working directory
    
    since this is a mixed ts / go project need to run it in a
    specific dir
    
    * remove path
    
    * format
    
    * add zed settings

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,4 +1,10 @@
-import { render } from 'preact';
-import { App } from './app';
+import { render } from "preact";
+import { App } from "./app";
 
-render(<App />, document.getElementById('app')!);
+const el = document.getElementById("app");
+
+if (!el) {
+	throw new Error("could not find element to render app");
+}
+
+render(<App />, el);
