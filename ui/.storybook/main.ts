022fb61d446b177f7ad77eae214449ba1010d56c
James Barzegar <james.barzegar@gmail.com>
2026-02-02 23:06:22 -0500

    fmt

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,10 +1,8 @@
-import type { StorybookConfig } from '@storybook/preact-vite';
+import type { StorybookConfig } from "@storybook/preact-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [],
-  "framework": "@storybook/preact-vite"
+	stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [],
+	framework: "@storybook/preact-vite",
 };
 export default config;
